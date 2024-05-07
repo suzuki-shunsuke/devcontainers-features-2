ce902e163f9858b1d1471039f4e111028b72a92d
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-05-07 20:03:20 +0900

    update

--- src/aqua-installer/install.sh
+++ src/aqua-installer/install.sh
@@ -38,4 +38,23 @@ if ! has_command curl && ! has_command wget; then
     fi
 fi
 
-"$(dirname $0)/aqua-installer"
+pwd
+ls
+
+url=https://raw.githubusercontent.com/aquaproj/aqua-installer/v3.0.0/aqua-installer
+
+tempdir=$(mktemp -d)
+cd "$tempdir"
+
+if has_command curl; then
+    curl -sSfL -O "$url"
+elif has_command wget; then
+    wget "$url"
+fi
+
+echo "8299de6c19a8ff6b2cc6ac69669cf9e12a96cece385658310aea4f4646a5496d  aqua-installer" | sha256sum -c
+
+chmod +x aqua-installer
+./aqua-installer
+
+rm -R "$tempdir"
