df508e6808424a1732d442e75dbc2739c91b13d0
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-05-07 19:11:03 +0900

    update

--- src/aqua-installer/install.sh
+++ src/aqua-installer/install.sh
@@ -1,3 +1,41 @@
-cp "$(dirname $0)/aqua-installer" /usr/local/bin/aqua-installer
+set -eu
 
-chmod +x /usr/local/share/aqua-installer
+log_error() {
+    echo "[ERROR] $1" >&2
+}
+
+if [ -z "${AQUA_VERSION:-}" ]; then
+    log_error "AQUA_VERSION is required"
+    exit 1
+fi
+
+has_command() {
+    local cmd=$1
+    command -v $cmd > /dev/null 2>&1
+}
+
+if ! has_command bash; then
+    if has_command apt-get; then
+        apt-get update -y
+        apt-get install -y bash
+    elif has_command apk > /dev/null 2>&1; then
+        apk add bash
+    else
+        log_error "bash isn't found. Please install bash to run aqua-installer"
+        exit 1
+    fi
+fi
+
+if ! has_command curl && ! has_command wget; then
+    if has_command apt-get; then
+        apt-get update -y
+        apt-get install -y curl
+    elif has_command apk > /dev/null 2>&1; then
+        apk add curl
+    else
+        log_error "Neither curl nor wget is found. Please install either curl or wget to download aqua"
+        exit 1
+    fi
+fi
+
+"$(dirname $0)/aqua-installer"
